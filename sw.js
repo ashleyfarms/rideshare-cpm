5b1c7ba0f3ee4e6af949ea5e9cc8afab2ba6fd9c
ashleyfarms <mcarfab@proton.me>
2026-09-06 23:47:13 -0500

    Cache app.js in the service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'rscpm-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+const CACHE = 'rscpm-v2';
+const ASSETS = ['./', './index.html', './app.js', './manifest.json', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
